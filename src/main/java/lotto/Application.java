82e5edc14a837ace1763a9d535d59004879dad1d
sunny.park <seolseee@gmail.com>
2024-10-01 17:34:27 +0900

    [feat] 메인 (사용자 입력받음)

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,35 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            System.out.println("구입금액을 입력해 주세요.");
+            int money = Integer.parseInt(Console.readLine());
+
+            LottoGame lottoGame = new LottoGame(money);
+
+            System.out.println("당첨 번호를 입력해 주세요.");
+            List<Integer> winningNumbers = Arrays.stream(Console.readLine().split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+
+            System.out.println("보너스 번호를 입력해 주세요.");
+            int bonusNumber = Integer.parseInt(Console.readLine());
+
+            WinningNumbers winningLotto = new WinningNumbers(winningNumbers, bonusNumber);
+            lottoGame.printStatistics(winningLotto);
+
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 잘못된 입력입니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
