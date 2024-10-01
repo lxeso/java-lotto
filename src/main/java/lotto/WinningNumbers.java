7dec9ebf7f2bb51e500f5fae702f179f5ee9585e
sunny.park <seolseee@gmail.com>
2024-10-01 17:33:02 +0900

    [feat] 당첨 번호, 보너스 번호 관리 클래스 (당첨번호 비교)

---
+++ src/main/java/lotto/WinningNumbers.java
@@ -0,0 +1,36 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningNumbers {
+    private final List<Integer> numbers;
+    private final int bonusNumber;
+
+    public WinningNumbers(List<Integer> numbers, int bonusNumber) {
+        validate(numbers, bonusNumber);
+        this.numbers = numbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, int bonusNumber) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 합니다.");
+        }
+        if (numbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이여야 합니다.");
+        }
+    }
+
+    public int matchCount(Lotto lotto) {
+        return (int) lotto.getNumbers().stream()
+                .filter(numbers::contains)
+                .count();
+    }
+
+    public boolean matchBonus(Lotto lotto) {
+        return lotto.getNumbers().contains(bonusNumber);
+    }
+}
